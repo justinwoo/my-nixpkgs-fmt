3ef6bbaf726e4c569e0dc079652b0259e1639620
justinwoo <moomoowoo@gmail.com>
2019-09-08 00:50:29 +0300

    update

--- default.nix
+++ default.nix
@@ -8,8 +8,8 @@ pkgs.stdenv.mkDerivation rec {
   name = "nixpkgs-fmt";
 
   src = pkgs.fetchurl {
-    url = "https://github.com/justinwoo/my-nixpkgs-fmt/releases/download/20190828/nixpkgs-fmt.tar.gz";
-    sha256 = "0ivhnmq9giy1sjw4nw5h6q06rskra4nyza89vvk7gal8mi818vcb";
+    url = "https://github.com/justinwoo/my-nixpkgs-fmt/releases/download/20190908/nixpkgs-fmt.tar.gz";
+    sha256 = "072vdnjn9f08j6364zjif5zybj5qcpwdcrvrmlrg8ikimj37xw8i";
   };
 
   buildInputs = [ pkgs.glibc ];
